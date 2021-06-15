e2831d234b2d544c86c80e3efcd1a10a2c41ff9d
448197683 <82860436+448197683@users.noreply.github.com>
2021-06-15 13:06:49 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+//必须用colorArray
+//用if, else
+//handleResize上添加code就OK
+
+const colorArray = [
+  'red',
+  'lightcyan',
+  'lightseagreen',
+  'tomato',
+  'cornflowerblue',
+  'gray',
+];
+
+const target = document.querySelector('body');
+
+function handleResize() {}
+
+window.addEventListener('resize', handleResize);
