d7e95057c301eeb5e6b8911927f65fa4ae0c15a4
448197683 <82860436+448197683@users.noreply.github.com>
2021-06-15 16:00:12 +0800

    finish

--- index.js
+++ index.js
@@ -13,6 +13,22 @@ const colorArray = [
 
 const target = document.querySelector('body');
 
-function handleResize() {}
+function handleResize() {
+  let w = window.innerWidth
+  console.log(w)
+  if(w<200){
+    target.style.backgroundColor = `red`
+  }else if(w>=200&&w<300){
+    target.style.backgroundColor = `lightcyan`
+  }else if(w>=300&&w<500){
+    target.style.backgroundColor = `lightseagreen`
+  }else if(w>=500&&w<700){
+    target.style.backgroundColor = `tomato`
+  }else if(w>=700&&w<900){
+    target.style.backgroundColor = `cornflowerblue`
+  }else{
+    target.style.backgroundColor = `gray`
+  }
+}
 
 window.addEventListener('resize', handleResize);
